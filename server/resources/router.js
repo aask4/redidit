7af571adcd0e69a410d6269ac9dd0f7dd1b3f2a8
Alex Jaynes <dr.ocheyo@gmail.com>
2018-02-22 15:04:03 -0800

    content controllers and helpers work from route to db

--- server/resources/router.js
+++ server/resources/router.js
@@ -1,27 +1,27 @@
 const router = require('express').Router();
-const controller = require('./controller');
+const controller = require('./contentController');
 
 // Ordered alphabetically by route
 router.route('/content')
   .get(controller.retreiveContent)
   .post(controller.createContent)
-  .put(controller.updateContent);
+//   .put(controller.updateContent);
 
-router.route('/login')
-  .get(controller.userLogin);
+// router.route('/login')
+//   .get(controller.userLogin);
 
-router.route('/signup')
-  .post(controller.userSignup);
+// router.route('/signup')
+//   .post(controller.userSignup);
 
-router.route('/subredidit')
-  .get(controller.retreiveSubredidit)
-  .post(controller.createSubredidit);
+// router.route('/subredidit')
+//   .get(controller.retreiveSubredidit)
+//   .post(controller.createSubredidit);
 
-router.route('/userprofile')
-  .get(controller.retreiveUserProfile);
+// router.route('/userprofile')
+//   .get(controller.retreiveUserProfile);
 
-router.route('/userprofile/subscription')
-  .get(controller.retreiveUserSubscriptions)
-  .post(controller.appendUserSubscription);
+// router.route('/userprofile/subscription')
+//   .get(controller.retreiveUserSubscriptions)
+//   .post(controller.appendUserSubscription);
 
 module.exports.router = router;
