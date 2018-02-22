e80c86383daa67598efbf3371bf00dcbc32d103a
Kin Chan <xkinchanx@gmail.com>
2018-02-21 19:34:18 -0800

    fixes to DB, added user/pw and framework fro subredidit

--- db/index.js
+++ db/index.js
@@ -1,18 +1,18 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('redidit', 'username', 'password', { //enter your username and password here
+const db = new Sequelize('redidit', 'redidit', 'redidit', { //enter your username and password here
   host: 'localhost',
   dialect: 'postgres',
-
+  
   pool: {
     max: 5,
     min: 0,
-    axquire: 30000,
-    idle, 10000
+    acquire: 30000,
+    idle: 10000
   },
-})
+});
 
 db.authenticate()
-  .then( () => { console.log('redidit database is connected')})
+  .then( () => console.log('redidit database is connected'))
   .catch( err => console.log('Error: ', err));
 
 module.exports = {db, Sequelize};
