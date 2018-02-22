5d9ed56c2820853fb369bf44f791f06af4a50f24
Aaron Valdez <valdez2010@gmail.com>
2018-02-22 14:44:40 -0800

    Added usersController to users and users/subscription routes.

--- server/resources/router.js
+++ server/resources/router.js
@@ -1,27 +1,28 @@
 const router = require('express').Router();
 const controller = require('./controller');
+const usersController = require('./usersController');
 
 // Ordered alphabetically by route
-router.route('/content')
-  .get(controller.retreiveContent)
-  .post(controller.createContent)
-  .put(controller.updateContent);
+// router.route('/content')
+//   .get(controller.retreiveContent)
+//   .post(controller.createContent)
+//   .put(controller.updateContent);
 
-router.route('/login')
-  .get(controller.userLogin);
+// router.route('/login')
+//   .get(controller.userLogin);
 
-router.route('/signup')
-  .post(controller.userSignup);
+// router.route('/signup')
+//   .post(controller.userSignup);
 
-router.route('/subredidit')
-  .get(controller.retreiveSubredidit)
-  .post(controller.createSubredidit);
+// router.route('/subredidit')
+//   .get(controller.retreiveSubredidit)
+//   .post(controller.createSubredidit);
 
 router.route('/userprofile')
-  .get(controller.retreiveUserProfile);
+  .get(usersController.fetchUserProfile);
 
 router.route('/userprofile/subscription')
-  .get(controller.retreiveUserSubscriptions)
-  .post(controller.appendUserSubscription);
+  .get(usersController.fetchUserSubscription)
+  .post(usersController.createUserSubscription);
 
 module.exports.router = router;
