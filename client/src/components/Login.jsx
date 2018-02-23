8f6f4e611a9859bf58163a04beb13169b818a73e
qiang song <qiangsong889@yahoo.com>
2018-02-22 18:13:50 -0800

    fixed Navbar component signup and login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,18 +1,19 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import axios from 'axios'
-import { addActiveUser } from '../actions'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import axios from "axios";
+import { addActiveUser } from "../actions";
 class Login extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      username: '',
-      password: ''
-    }
+      username: "",
+      password: ""
+    };
   }
   handleLoginButtonClick() {
-    console.log('button clicked', this.state)
+    console.log("button clicked", this.state);
+    this.props.addActiveUser("shayne");
     // axios.get('api/login'), {params: this.state}
     //   .then(res=> {
     //       this.props.addActiveUser(res.data[0])
@@ -21,30 +22,50 @@ class Login extends Component {
   }
   onchangeHandler(e) {
     this.setState({
-      [e.target.name]: e.target.value,
-    })
+      [e.target.name]: e.target.value
+    });
   }
   render() {
     return (
-      <div style={{float:'right'}}>
-        <input name='username' type='text' id='username' placeholder="Username"  onChange={(e)=> this.onchangeHandler(e)} required/>
-        <input type='password' name='password' placeholder="Password" onChange={(e)=> this.onchangeHandler(e)} required/>
-        <br/>
-        <button style={{float: 'right'}} onClick={() => this.handleLoginButtonClick()}>login</button>
+      <div style={{ float: "right" }}>
+        <input
+          name="username"
+          type="text"
+          id="username"
+          placeholder="Username"
+          onChange={e => this.onchangeHandler(e)}
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={e => this.onchangeHandler(e)}
+          required
+        />
+        <br />
+        <button
+          style={{ float: "right" }}
+          onClick={() => this.handleLoginButtonClick()}
+        >
+          login
+        </button>
       </div>
-    )
+    );
   }
 }
-function mapStateToProps (state) {
+function mapStateToProps(state) {
   return {
     active_user: state.active_user
-  }
+  };
 }
 function matchDispatchToProps(dispatch) {
-  return bindActionCreators({
-    addActiveUser: addActiveUser
-  },dispatch)
+  return bindActionCreators(
+    {
+      addActiveUser: addActiveUser
+    },
+    dispatch
+  );
 }
 
-export default connect(mapStateToProps, matchDispatchToProps)(Login)
-
+export default connect(mapStateToProps, matchDispatchToProps)(Login);
--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,41 +1,75 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { addActiveUser } from "../actions";
 class SignUp extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      email: '',
-      username: '',
-      password: '',
-      submitEmail:false
-    }
+      email: "",
+      username: "",
+      password: "",
+      submitEmail: false
+    };
   }
   handleInput(e) {
     this.setState({
       [e.target.name]: e.target.value
-    })
+    });
   }
   handleSubmit() {
+    console.log(this.props.history.push("/"));
+    this.props.addActiveUser("shayne");
   }
   render() {
-    return(
+    return (
       <div>
-        {
-          this.state.submitEmail ? 
+        {this.state.submitEmail ? (
           <div>
-          Username :<input type='text' name='username' onChange={(e)=>this.handleInput(e)}/><br/>
-          Password :<input type="password" name='password' onChange={(e)=>this.handleInput(e)}/><br/>
-          <button onClick={()=> this.handleSubmit()}>Submit</button>
+            Username :<input
+              type="text"
+              name="username"
+              onChange={e => this.handleInput(e)}
+            />
+            <br />
+            Password :<input
+              type="password"
+              name="password"
+              onChange={e => this.handleInput(e)}
+            />
+            <br />
+            <button onClick={() => this.handleSubmit()}>Submit</button>
           </div>
-          :
+        ) : (
           <div>
-          Email:<br/><input  name='email' onChange={(e)=>this.handleInput(e)}/>
-          <button onClick={()=> {this.setState({submitEmail: true}), console.log('email submit button click', this.state)}}>submit</button>
+            Email:<br />
+            <input name="email" onChange={e => this.handleInput(e)} />
+            <button
+              onClick={() => {
+                this.setState({ submitEmail: true }),
+                  console.log("email submit button click", this.state);
+              }}
+            >
+              submit
+            </button>
           </div>
-        }
+        )}
       </div>
-    )
+    );
   }
 }
+function mapStateToProps(state) {
+  return {
+    active_user: state.active_user
+  };
+}
+function matchDispatchToProps(dispatch) {
+  return bindActionCreators(
+    {
+      addActiveUser: addActiveUser
+    },
+    dispatch
+  );
+}
 
-export default SignUp
+export default connect(mapStateToProps, matchDispatchToProps)(SignUp);
