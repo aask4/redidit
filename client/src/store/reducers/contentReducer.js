52bd7f3c4af25a7fd8bfbd12fae892fede08790a
Alex Jaynes <dr.ocheyo@gmail.com>
2018-02-22 18:27:22 -0800

    tryign to build reducers (wrong)

--- client/src/store/reducers/contentReducer.js
+++ client/src/store/reducers/contentReducer.js
@@ -3,7 +3,9 @@ import Axios from 'axios';
 export default function (queryObj) {
   queryObj.type = queryObj.type || 'post';
   let data;
-  Axios.get('/content', queryObj)
+  Axios.get('/content', {
+      params: queryObj
+    })
     .then((result) => data = result)
     .catch((err) => console.log('Error in contentReducer.get: ', err));
   return data;
