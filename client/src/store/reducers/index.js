fe8807e9c06a02fbb1883cfe4891c547273359d6
Aaron Valdez <valdez2010@gmail.com>
2018-02-23 14:45:24 -0800

    Added userprofile skeleton and selected user reducer and action.

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,8 +1,12 @@
-import { combineReducers } from 'redux'
-import test from './testReducers'
-import active_user from './activeUserReducers'
+import { combineReducers } from 'redux';
+import test from './testReducers';
+import active_user from './activeUserReducers';
+import selectedUser from './selectedUserReducers';
+import current_posts from './currentPostsReducers';
 
 export default combineReducers({
-    test: test,
-    active_user: active_user
-})
+  test,
+  active_user,
+  selectedUser,
+  current_posts,
+});
