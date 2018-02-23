a8b79584bfb51fba49d59e988feb99b4a5ead2ac
Alex Jaynes <dr.ocheyo@gmail.com>
2018-02-23 11:16:30 -0800

    styling changes

--- db/models/contentModel.js
+++ db/models/contentModel.js
@@ -1,16 +1,17 @@
-const { db, Sequelize } = require("../index");
+const { db, Sequelize } = require('../index');
+const User = require('./usersModel');
 
-const Content = db.define("Contents", {
+const Content = db.define('Contents', {
   content: Sequelize.STRING,
   score: Sequelize.INTEGER,
-  type: Sequelize.STRING
+  type: Sequelize.STRING,
 });
 
-Content.hasOne(User, { as: "owner" });
-Content.hasOne(Content, { as: "parent" });
+Content.hasOne(User, { as: 'owner' });
+Content.hasOne(Content, { as: 'parent' });
 
 Content.sync()
   .then(() => console.log("Content has been sync'd"))
-  .catch(err => console.log("Error on model: ", err));
+  .catch(err => console.log('Error on model: ', err));
 
 module.exports = Content;
