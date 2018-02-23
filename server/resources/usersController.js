4fb37aee01181abc19ec48c05e2293d1c0f8e612
Kin Chan <xkinchanx@gmail.com>
2018-02-22 17:16:53 -0800

    added and commented out new functions and paths for user access to user/subscription model.  Uncomment to use

--- server/resources/usersController.js
+++ server/resources/usersController.js
@@ -1,5 +1,6 @@
 const Users = require('../../db/models/usersModel');
 // const Subredidits = require('../../db/models/subrediditModel');
+// const UsersSubredidits = require('../../db/models/usersSubrediditModel');
 
 module.exports.fetchUserProfile = (req, res) => {
   Users.findAll({ where: { username: req.query.username } })
@@ -15,9 +16,31 @@ module.exports.fetchUserSubscription = (req, res) => {
       res.end(JSON.stringify(user));
     })
     .catch(err => res.end(err));
+
+  //Not sure if you need this
+  // UsersSubredidits.findAll({where: {users_id: userID, subredidits_id: subredidits_id}})
+  //   .then( result => res.status(200).send(result))
+  //   .catch( err => res.status(200).send('Error'))
 };
 
-module.exports.createUserSubscription = (req, res) => {};
+module.exports.createUserSubscription = (req, res) => {
+
+  //TODO: please update parameters
+  // let newSubscription = UsersSubredidits.create({
+  //   users_id: userID,
+  //   subredidits_id: subrediditID
+  // })
+  // .then( result => res.status(201).send('Added'))
+  // .catch( err => res.status(201).send('Error'))
+};
+
+module.exports.deleteUserSubscription = (req, res) => {
+
+  //TODO: please update parameters
+  // UsersSubredidits.destroy({where: {users_id: userID, subredidits_id: subredidits_id}})
+  // .then( result => res.status(200).send(result))
+  // .catch( err => res.status(200).send('Error'))
+}
 
 // module.exports.createUser = (req, res) => {
 //   Users.create({
