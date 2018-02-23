0e496f2c6870a83ee5d2b46bf51dcff44929ac23
Aaron Valdez <valdez2010@gmail.com>
2018-02-22 16:37:43 -0800

    Fixed content duplicates.

--- db/helpers/contentHelpers.js
+++ db/helpers/contentHelpers.js
@@ -1,24 +1,31 @@
-const Content =require('./contentSchema');
+const Content =require('./contentModel');
 
-module.export.postContent = (contentObj, callback) => {
+module.exports.postContent = (contentObj, callback) => {
+  console.log(contentObj);
   Content.create({
     owner: contentObj.owner,
     content: contentObj.content,
     score: 0,
-    parent: contentObj.parent
+    parent: contentObj.parent,
+    type: contentObj.type
   })
-    .then((result) => console.log('Saved to database: ', result))
+    .then((result) => {
+      console.log('Saved to database: ', result);
+      callback(result);
+    })
     .catch((err) => console.log('Error in postContent: ', err))
 };
 
-module.export.getContent = (queryObj, callback) => {
+module.exports.getContent = (queryObj, callback) => {
   Content.findAll({where: queryObj})
     .then((result) => callback(result))
     .catch((err) => console.log('Error in getContent: ', err));
 };
 
-module.export.updateContent = (contentObj, callback) => {
+module.exports.updateContent = (contentObj, callback) => {
   Content.update({score: contentObj.score}, {where: {id: contentObj.id}})
     .then((result) => console.log('Content updated.'))
     .catch((err) => console.log('Error in updateContent: ', err));
 };
+
+module.exports
