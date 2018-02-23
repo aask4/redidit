41dca2aeb60338076f16d2246052235cece300bb
Kin Chan <xkinchanx@gmail.com>
2018-02-22 20:00:52 -0800

    fixing merging issues

--- server/resources/usersController.js
+++ server/resources/usersController.js
@@ -1,14 +1,10 @@
-const Users = require("../../db/models/usersModel");
+const Users = require('../../db/models/usersModel');
 // const Subredidits = require('../../db/models/subrediditModel');
-<<<<<<< HEAD
-const UsersSubredidits = require("../../db/models/usersSubrediditModel");
-=======
-// const UsersSubredidits = require('../../db/models/usersSubrediditModel');
->>>>>>> 90bb515886dcc4acf95d2bd1989325da5fa839eb
+const UsersSubredidits = require('../../db/models/usersSubrediditModel');
 
 module.exports.fetchUserProfile = (req, res) => {
   Users.findAll({ where: { username: req.query.username } })
-    .then(user => {
+    .then((user) => {
       res.end(JSON.stringify(user));
     })
     .catch(err => res.end(err));
@@ -16,20 +12,31 @@ module.exports.fetchUserProfile = (req, res) => {
 
 module.exports.fetchUserSubscription = (req, res) => {
   Users.findAll({ where: { username: req.query.username } })
-    .then(user => {
+    .then((user) => {
       res.end(JSON.stringify(user));
     })
     .catch(err => res.end(err));
 
-  //Not sure if you need this
+  // Not sure if you need this
   // UsersSubredidits.findAll({where: {users_id: userID, subredidits_id: subredidits_id}})
   //   .then( result => res.status(200).send(result))
   //   .catch( err => res.status(200).send('Error'))
-<<<<<<< HEAD
 };
 
 module.exports.createUserSubscription = (req, res) => {
-  //TODO: please update parameters
+  // TODO: please update parameters
+  // let newSubscription = UsersSubredidits.create({
+  //   users_id: userID,
+  //   subredidits_id: subrediditID
+  // })
+  // .then( result => res.status(201).send('Added'))
+  // .catch( err => res.status(201).send('Error'))
+};
+
+module.exports.deleteUserSubscription = (req, res) => {};
+
+module.exports.createUserSubscription = (req, res) => {
+  // TODO: please update parameters
   // let newSubscription = UsersSubredidits.create({
   //   users_id: userID,
   //   subredidits_id: subrediditID
@@ -39,32 +46,11 @@ module.exports.createUserSubscription = (req, res) => {
 };
 
 module.exports.deleteUserSubscription = (req, res) => {
-=======
-};
-
-module.exports.createUserSubscription = (req, res) => {
-
-  //TODO: please update parameters
-  // let newSubscription = UsersSubredidits.create({
-  //   users_id: userID,
-  //   subredidits_id: subrediditID
-  // })
-  // .then( result => res.status(201).send('Added'))
-  // .catch( err => res.status(201).send('Error'))
-};
-
-module.exports.deleteUserSubscription = (req, res) => {
-
->>>>>>> 90bb515886dcc4acf95d2bd1989325da5fa839eb
-  //TODO: please update parameters
+  // TODO: please update parameters
   // UsersSubredidits.destroy({where: {users_id: userID, subredidits_id: subredidits_id}})
   // .then( result => res.status(200).send(result))
   // .catch( err => res.status(200).send('Error'))
-<<<<<<< HEAD
 };
-=======
-}
->>>>>>> 90bb515886dcc4acf95d2bd1989325da5fa839eb
 
 // module.exports.createUser = (req, res) => {
 //   Users.create({
