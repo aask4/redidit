7458de3036f786299bdaaa79e9685a12af4dae2f
Kin Chan <xkinchanx@gmail.com>
2018-02-24 15:31:03 -0800

    filled out functionality for subscribe button and redux.

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 import Nav from './Nav.jsx';
 import Search from './Search.jsx';
 import ContentList from './ContentList.jsx';
+import SubscribeButton from './SubscribeButton';
 
 class Main extends Component {
   render() {
     return (
       <div>
-        hello from main
+        Main: insert subredit name here
+        <SubscribeButton />
         <div>{/* <Nav /> can add hot + new as needed */}</div>
         <div>{/* user options on right including logout */}</div>
-        <div>{/* <Search /> */}</div>
+        <div>
+          <Search />
+        </div>
         <div>
           <ContentList />
         </div>
