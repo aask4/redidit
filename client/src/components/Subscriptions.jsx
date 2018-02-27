551bee5815c8e8f4f977336a74daa99150a2cc8f
Kin Chan <xkinchanx@gmail.com>
2018-02-26 23:36:38 -0800

    personalized dropdown available for subscriptions upon login.  Need to work on rendering related posts upon picking a subscription

--- client/src/components/Subscriptions.jsx
+++ client/src/components/Subscriptions.jsx
@@ -1,33 +1,46 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { addActiveSubredidit } from '../actions/index';
+import React from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { addActiveSubredidit } from "../actions/index";
 
 class Subscriptions extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  selectSubredidit(sub) {
-    console.log(sub);
-    // this.props.addActiveSubredidit(sub)
+  selectSubredidit(event) {
+    let subName = event.target.value;
+    let subredidit;
+
+    this.props.subredidits.forEach(sub => {
+      if (sub.name === subName) {
+        subredidit = sub;
+      }
+    });
+    this.props.addActiveSubredidit(subredidit);
   }
 
   render() {
     return (
       <div>
         {this.props.active_user ? (
-          <div>
-            MySubscriptions:
-            {/* <button type="button" onClick={() => this.selectSubredidit()} /> */}
-            {/* <select name="userSubscriptions">
-              {this.props.active_user.subredidits.map(sub => (
-                <option onClick={() => this.selectSubredidit(sub)} value={sub.name}>
-                  {sub.name}
-                </option>
-              ))}
-            </select> */}
-          </div>
+          this.props.active_user.subredidit ? (
+            <div>
+              <select
+                name="userSubscriptions"
+                onChange={e => this.selectSubredidit(e)}
+              >
+                <option value="main">MySubscriptions</option>
+                {this.props.active_user.subredidit.map(sub => (
+                  <option key={sub.id} value={sub.name}>
+                    {sub.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+          ) : (
+            <div />
+          )
         ) : (
           <div />
         )}
@@ -39,15 +52,16 @@ class Subscriptions extends React.Component {
 function mapStateToProps(state) {
   return {
     active_user: state.active_user,
+    subredidits: state.all_subredidit
   };
 }
 
 function matchDispatchToProps(dispatch) {
   return bindActionCreators(
     {
-      addActiveSubredidit,
+      addActiveSubredidit
     },
-    dispatch,
+    dispatch
   );
 }
 
