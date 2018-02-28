82179b87461dcb23e8ae3ef7b54962fe43c0cc09
Kin Chan <xkinchanx@gmail.com>
2018-02-27 17:05:05 -0800

    creating new reducer to track user subscriptions

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,10 +1,11 @@
-import { combineReducers } from 'redux';
-import test from './testReducers';
-import active_user from './activeUserReducers';
-import selectedUser from './selectedUserReducers';
-import current_posts from './currentPostsReducers';
-import active_subredidit from './activeSubrediditReducers';
-import all_subredidit from './allSubrediditReducers';
+import { combineReducers } from "redux";
+import test from "./testReducers";
+import active_user from "./activeUserReducers";
+import selectedUser from "./selectedUserReducers";
+import current_posts from "./currentPostsReducers";
+import active_subredidit from "./activeSubrediditReducers";
+import all_subredidit from "./allSubrediditReducers";
+import active_user_subredidit from "./activeUserSubrediditReducers";
 
 export default combineReducers({
   test,
@@ -13,4 +14,5 @@ export default combineReducers({
   current_posts,
   active_subredidit,
   all_subredidit,
+  active_user_subredidit
 });
