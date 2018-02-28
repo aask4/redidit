ce5f3767417ba835ddec5da45c211f97683b09d0
Aaron Valdez <valdez2010@gmail.com>
2018-02-28 10:44:15 -0800

    Linked active user to userprofile.

--- client/src/components/StatusBar.jsx
+++ client/src/components/StatusBar.jsx
@@ -1,57 +1,69 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import SignUp from "./SignUp";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { addActiveUser } from "../actions";
-import { bindActionCreators } from "redux";
-import firebase from "firebase";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import SignUp from './SignUp';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { addActiveUser, selectUser } from '../actions';
+import { bindActionCreators } from 'redux';
+import firebase from 'firebase';
+
 class StatusBar extends Component {
   constructor(props) {
     super(props);
+    this.selectUserHandler = this.selectUserHandler.bind(this);
   }
+
   signupClick() {
-    console.log("button clicked", this.props.history.push("/signup"));
+    console.log('button clicked', this.props.history.push('/signup'));
   }
+
   logout() {
-    let self = this;
-    console.log("log out button been clicked");
+    const self = this;
+    console.log('log out button been clicked');
     firebase
       .auth()
       .signOut()
-      .then(function() {
-        console.log("user signOut");
+      .then(() => {
+        console.log('user signOut');
         // Sign-out successful.
         self.props.addActiveUser(null);
       })
-      .catch(function(error) {
+      .catch((error) => {
         // An error happened.
-        console.log("error sign out", error);
+        console.log('error sign out', error);
       });
   }
+
+  selectUserHandler(event) {
+    this.props.selectUser(this.props.active_user.username);
+  }
+
   render() {
     return (
       <div>
         <div>
           <a href="/">
             <img
-              style={{ float: "left", position: "relative" }}
+              style={{ float: 'left', position: 'relative' }}
               className="logo"
               src="https://i.imgur.com/OX3vXmU.png"
             />
           </a>
         </div>
-        <div style={{ float: "right" }}>
+        <div style={{ float: 'right' }}>
           {this.props.active_user ? (
             <div>
-              {this.props.active_user.username}
+              <Link to="/userprofile" onClick={this.selectUserHandler}>
+                {this.props.active_user.username}
+              </Link>
               <button onClick={() => this.logout()}>logout</button>
             </div>
           ) : (
             <div>
-              want to join?{" "}
+              want to join?{' '}
               <a href="" onClick={() => this.signupClick()}>
                 <strong>sign up</strong>
-              </a>{" "}
+              </a>{' '}
               in seconds
             </div>
           )}
@@ -62,15 +74,17 @@ class StatusBar extends Component {
 }
 function mapStateToProps(state) {
   return {
-    active_user: state.active_user
+    active_user: state.active_user,
+    selectedUser: state.selectedUser,
   };
 }
 function matchDispatchToProps(dispatch) {
   return bindActionCreators(
     {
-      addActiveUser
+      addActiveUser,
+      selectUser,
     },
-    dispatch
+    dispatch,
   );
 }
 export default connect(mapStateToProps, matchDispatchToProps)(StatusBar);
