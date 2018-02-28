c3bbf5f50ca53d3022ad017a6076a77ceb57e017
Aaron Valdez <valdez2010@gmail.com>
2018-02-28 15:50:10 -0800

    Added dynamic url to db index.js.

--- db/index.js
+++ db/index.js
@@ -1,19 +1,37 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('redidit', 'redidit', 'redidit', {
-  host: 'localhost',
-  dialect: 'postgres',
-  logging: false,
-  
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
-});
 
-db.authenticate()
-  .then( () => console.log('redidit database is connected'))
-  .catch( err => console.log('Error: ', err));
+let db;
+if (process.env.DATABASE_URL) {
+  db = new Sequelize(process.env.DATABASE_URL, {
+    host: 'localhost',
+    dialect: 'postgres',
+    logging: false,
 
-module.exports = {db, Sequelize};
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  });
+} else {
+  db = new Sequelize('redidit', 'redidit', 'redidit', {
+    host: 'localhost',
+    dialect: 'postgres',
+    logging: false,
+
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  });
+}
+
+db
+  .authenticate()
+  .then(() => console.log('redidit database is connected'))
+  .catch(err => console.log('Error: ', err));
+
+module.exports = { db, Sequelize };
