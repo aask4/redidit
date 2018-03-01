041a21f39cbe261d17e50b71e22a2885ef10e7d8
Aaron Valdez <valdez2010@gmail.com>
2018-02-28 16:01:27 -0800

    Added userprofile route and fixed wildcard.

--- server/resources/router.js
+++ server/resources/router.js
@@ -1,32 +1,31 @@
-const router = require("express").Router();
-const contentController = require("./contentController");
-// const controller = require('./controller');
-const usersController = require("./usersController");
-const subrediditController = require("./subrediditController");
-const subscriptionController = require("./subscriptionController");
+const router = require('express').Router();
+const contentController = require('./contentController');
+const usersController = require('./usersController');
+const subrediditController = require('./subrediditController');
+const subscriptionController = require('./subscriptionController');
 
 router
-  .route("/content")
+  .route('/content')
   .get(contentController.retreiveContent)
   .post(contentController.createContent)
   .put(contentController.updateContent);
 
-router.route("/content/vote").get(contentController.getVotes);
-// .post(contentController.createVotes);
+router.route('/content/vote').get(contentController.getVotes);
 
 router
-  .route("/subredidit")
+  .route('/subredidit')
   .get(subrediditController.retrieveSubredidit)
   .post(subrediditController.createSubredidit);
 
 router
-  .route("/subscription")
-  .get(subscriptionController.fetchUserSubscription) // TODO: work with Shayne to merge with login
+  .route('/subscription')
+  .get(subscriptionController.fetchUserSubscription)
   .post(subscriptionController.createUserSubscription)
   .delete(subscriptionController.deleteUserSubscription);
 
-router.route("/signup").post(usersController.createUser);
-router.route("/login").get(usersController.login);
-router.route("/authentication").get(usersController.authentication);
+router.route('/signup').post(usersController.createUser);
+router.route('/login').get(usersController.login);
+router.route('/authentication').get(usersController.authentication);
+router.route('/userprofile/:user').get((req, res) => res.redirect('/'));
 
 module.exports.router = router;
